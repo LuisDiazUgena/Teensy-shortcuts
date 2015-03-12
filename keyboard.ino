e6db6b6c0feabe8b70f4fda4c8d84d17706e3b16
Luis Diaz <luisdiazugena@gmail.com>
2015-03-12 12:10:27 +0100

    Added functions: selectProfile, printProfile, profileActions

--- keyboard.ino
+++ keyboard.ino
@@ -4,28 +4,97 @@
 #include <Bounce.h>
 #include <usb_keyboard.h>
 
-int pinBtn1 = 15,pinBtn2 = 14;
+int pinBtn1 = 15,pinBtn2 = 14, pinProfileSelector = A2;
+
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
 
-//Crea botones con debounce por software
+//Create buttons with software debounce
 Bounce btn1 = Bounce(pinBtn1,10);
 Bounce btn2 = Bounce(pinBtn2,10);
 
+//Profile Selector
+int aktProfile,profile_number=2;
+
+String profile1 = "Arduino IDE", profile2 = "Inkscape";
+
 void setup() {
+
+  pinMode(pinBtn1, INPUT_PULLUP);
+  pinMode(pinBtn2, INPUT_PULLUP);
+  pinMode(pinProfileSelector, INPUT);
 
   // set up the LCD's number of columns and rows: 
   lcd.begin(20,4);
-  
-  pinMode(pinBtn1, INPUT_PULLUP);
-  pinMode(pinBtn2, INPUT_PULLUP);
+
   // Print a message to the LCD.
   lcd.print("Teensy Shorcuts!");
-  delay(1000);
+  lcd.setCursor(0,3);
+  lcd.print("Luis Diaz");
+  delay(750);
   lcd.clear();
+
 }
 
 void loop() {
+
+  aktProfile = selectProfile();
+  printProfile(aktProfile);
+  profileActions(aktProfile);
+
+}
+
+void profileActions(int aktProfile){
+
+  btn1.update();
+  btn2.update();
+
+  switch (aktProfile) {
+    case 1:
+     if (btn1.risingEdge()){
+        Serial.println("risingEdge btn1");
+        Keyboard.set_modifier(MODIFIERKEY_CTRL);
+        Keyboard.set_key1(KEY_R);
+        Keyboard.send_now();
+
+     }
+
+    if (btn2.risingEdge()){
+      Serial.println("risingEdge btn2");
+      Keyboard.set_modifier(MODIFIERKEY_CTRL);
+      Keyboard.set_key1(KEY_U);
+      Keyboard.send_now();
+    }          // do something
+    break;
+    case 2:
+      // TODO --> add inkscape shorcuts
+      break;
+
+  }
+
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+
+}
+
+int selectProfile(){
+
+  int trimmer = 0;
+
+  trimmer = analogRead(pinProfileSelector);
+
+  if (trimmer < 1023/profile_number){
+    return 1;
+  }
+  else{
+    return 2;
+  }
+
+}
+
+void printProfile(int aktProfile){
+
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 0);
@@ -35,28 +104,14 @@ void loop() {
 
   //Funcion que modifque los perfiles -- todo --
 
-  lcd.print("Arduino IDE");
-
-  btn1.update();
-  btn2.update();
-  
-  if (btn1.risingEdge()){
-    Serial.println("risingEdge btn1");
-    Keyboard.set_modifier(MODIFIERKEY_CTRL);
-    Keyboard.set_key1(KEY_R);
-    Keyboard.send_now();
-
+  switch (aktProfile) {
+    case 1:
+    lcd.print(profile1);
+    break;
+    case 2:
+    lcd.print(profile2);
+    break;
   }
-  
-  if (btn2.risingEdge()){
-    Serial.println("risingEdge btn2");
-    Keyboard.set_modifier(MODIFIERKEY_CTRL);
-    Keyboard.set_key1(KEY_U);
-    Keyboard.send_now();
-
-  }  
-    Keyboard.set_modifier(0);
-    Keyboard.set_key1(0);
-    Keyboard.send_now();
-  
 }
+
+
